fd03250fd13d94d4a6e61fd53943cc9d6ab718de
willxu1234 <wlxu@ucsd.edu>
2019-10-26 23:21:35 -0700

    remove node_modules

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Container, Typography } from '@material-ui/core';
+import { Paper, Container, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
     root: {
